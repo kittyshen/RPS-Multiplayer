0e26ed39bac95b0011604e80602ed4186b6aec85
Kitty <kittykumas@gmail.com>
2018-05-28 13:46:47 -0700

    round 2 fighting against the connection and grab value out of snapshot of the firebase returned data.  still messy logic, but after click button the value changed now

--- assets/javascript/rpsApp.js
+++ assets/javascript/rpsApp.js
@@ -64,10 +64,11 @@ userListRef.on("value", function(snap) {
   counter = snap.numChildren();
   // console.log(counter);
 });
-
-database.ref(".info/connected")
-  .on("value", function (snap) {
-      if (snap.val()) {
+var index;
+var snapsaved;
+// database.ref(".info/connected")
+//   .on("value", function (snap) {
+//       if (snap.val()) {
         // console.log(snap.val());
         // if we lose network then remove this user from the list
         userListRef.once("value", 
@@ -76,151 +77,35 @@ database.ref(".info/connected")
           //The number of online users is the number of children in the connections list.
           counter = snap.numChildren();
           console.log(counter);
+          $("#player1").text(JSON.stringify(snap.val()))//+ "\n"+JSON.stringify(snap.val().child(index).win));
+
         }).then(function(){
           var connection = userListRef.child(counter+1).set({ 'id': counter+1, 'name': 'Kitty', 'win':4 });
       
           //postRef.push({ 'name': 'Test Value' })
           //var connection = userListRef.push({ 'id': 1, 'name': 'Kitty', 'win':4 });  //postRef.push({ 'name': 'Test Value' })
-          var index = userListRef.child(counter).key;
-          // console.log(userListRef.child(counter));
-          userListRef.on("childAdded", 
-          function(snap) { userListRef.child(index).onDisconnect().remove();
-          
-          })
-    
-          // console.log(connection);
-          tempkey = connection.key;
-          // $("#connected-viewers").text(JSON.stringify(con));
+          index = userListRef.child(counter).key;
+          userListRef.child(index).onDisconnect().remove();
+          console.log(userListRef.child(index));
+          userListRef.on("value", 
+          function(snap) {    
+            snapsaved = snap.val();
+            console.log(snapsaved);
+            $("#connected-viewers").text(JSON.stringify(snapsaved));
+            console.log(index);
+            $("#player1").text(snapsaved[index].name +":"+ snapsaved[index].id +":"+ snapsaved[index].win);
+          });
+
         });
-      }
-  });
-  userListRef.on("value", function(snap) {
-  
-    console.log(JSON.stringify(snap.val(),null,1));
-    //var tempObj = //JSON.parse(JSON.stringify(snap,null,1));
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
-    //$("#connected-viewers").text(JSON.stringify(snap.val(),null,1));
-    $("#connected-viewers").text(JSON.stringify(snap.val(),null,1));
-    // for(prop in tempObj){
-    //   // console.log(tempObj.prop);
-    //   // if(snap.prop.id == "1")
-    //   //   $("#player1").text(snap.prop.name);
-    // }
-    // console.log(tempObj[tempkey]);
-    $("#player1").text(JSON.stringify(snap.val().child("1").name)+ "\n"+JSON.stringify(snap.val().child("1").win));
+
+
     $(document).on("click", "#add-win",function(value){ 
       value.preventDefault();
       console.log("clickeded !");
       // console.log(userListRef);
-      userListRef.child("1").set({win : 6}); 
+      userListRef.child(index).update({win : 6}); 
      // userListRef.update({ 'id': 1, 'name': 'Kitty', 'win':5})
     });
 
-  });
 
   
-  // When first loaded or when the connections list changes...
-  // userListRef.on("value", function(snap) {
-  
-  //   // Display the viewer count in the html.
-  //   // The number of online users is the number of children in the connections list.
-  //   $("#connected-viewers").text(snap.numChildren());
-  // });
-  
-  // Number of online users is the number of objects in the presence list.
-  
-  
-  // ----------------------------------------------------------------
-  // At the page load and subsequent value changes, get a snapshot of the local data.
-  // This function allows you to update your page in real-time when the values within the firebase node bidderData changes
-  database.ref("/bidderData").on("value", function(snapshot) {
-  
-    // If Firebase has a highPrice and highBidder stored (first case)
-    if (snapshot.child("highBidderSub").exists() && snapshot.child("highPriceSub").exists()) {
-  
-      // Set the local variables for highBidder equal to the stored values in firebase.
-      highBidder = snapshot.val().highBidderSub;
-      highPrice = parseInt(snapshot.val().highPriceSub);
-  
-      // change the HTML to reflect the newly updated local values (most recent information from firebase)
-      $("#highest-bidder").text(highBidder);
-      $("#highest-price").text("$" + highPrice);
-  
-      // Print the local data to the console.
-      console.log(snapshot.val().highBidderSub);
-      console.log(snapshot.val().highPriceSub);
-    }
-  
-    // Else Firebase doesn't have a highPrice/highBidder, so use the initial local values.
-    else {
-  
-      // Change the HTML to reflect the local value in firebase
-      $("#highest-bidder").text(highBidder);
-      $("#highest-price").text("$" + highPrice);
-  
-      // Print the local data to the console.
-      console.log("local High Price");
-      console.log(highBidder);
-      console.log(highPrice);
-    }
-  
-  // If any errors are experienced, log them to console.
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
-  
-  // --------------------------------------------------------------
-  
-  // Whenever a user clicks the submit-bid button
-  $("#submit-bid").on("click", function(event) {
-    event.preventDefault();
-  
-    // Get the input values
-    var bidderName = $("#bidder-name").val().trim();
-    var bidderPrice = parseInt($("#bidder-price").val().trim());
-  
-    // Log to console the Bidder and Price (Even if not the highest)
-  
-  
-    if (bidderPrice > highPrice) {
-  
-      // Alert
-      alert("You are now the highest bidder.");
-  
-      // Save the new price in Firebase
-      database.ref("/bidderData").set({//"/bidderData"
-        highBidderSub: bidderName,
-        highPriceSub: bidderPrice
-      });
-  
-      // Log the new High Price
-      console.log("New High Price!");
-      console.log(bidderName);
-      console.log(bidderPrice);
-  
-      // Store the new high price and bidder name as a local variable (could have also used the Firebase variable)
-      highBidder = bidderName;
-      highPrice = parseInt(bidderPrice);
-  
-      // Change the HTML to reflect the new high price and bidder
-      $("#highest-bidder").text(bidderName);
-      $("#highest-price").text("$" + bidderPrice);
-  
-    }
-    else {
-  
-      // Alert
-      alert("Sorry that bid is too low. Try again.");
-    }
-  });
-
-  
-  //reference push()
-
-//   let postRef = firebase.database().ref('/post');
-// postRef.push({ 'name': 'Test Value' })
-//     .then(res => {
-//         console.log(res.getKey()) // this will return you ID
-//     })
-//     .catch(error => console.log(error));
